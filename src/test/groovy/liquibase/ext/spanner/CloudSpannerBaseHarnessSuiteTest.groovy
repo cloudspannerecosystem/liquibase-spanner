b580206a3038322b64e3aff43f65981f0a34dec9
Vladimir Evdokimov <Vladimir@maxxle.com>
2025-01-15 16:54:07 +0400

    revert data change test

--- src/test/groovy/liquibase/ext/spanner/CloudSpannerBaseHarnessSuiteTest.groovy
+++ src/test/groovy/liquibase/ext/spanner/CloudSpannerBaseHarnessSuiteTest.groovy
@@ -1,10 +1,6 @@
 package liquibase.ext.spanner
 
 import liquibase.harness.BaseHarnessSuite
-import liquibase.harness.change.ChangeObjectTests
-import liquibase.harness.snapshot.SnapshotObjectTests
-import org.junit.platform.suite.api.SelectClasses
 
-@SelectClasses(ChangeObjectTests.class)
 class CloudSpannerBaseHarnessSuiteTest extends BaseHarnessSuite {
 }
